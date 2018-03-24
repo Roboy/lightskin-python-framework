396a887321a8e33a0af5cc859df55950bed35ee2
Daniel Schubert <daniel.schubert@tum.de>
2018-03-24 22:33:47 +0100

    Parsing of Arduino messages

--- ArduinoConnectorForwardModel.py
+++ ArduinoConnectorForwardModel.py
@@ -15,9 +15,7 @@ class ArduinoConnectorForwardModel(ForwardModel):
 
         self._sensorValues = []
         for i in range(len(self.ls.LEDs)):
-            self._sensorValues.append([])
-            for j in range(len(self.ls.sensors)):
-                self._sensorValues[i].append(self.ls.forwardModel.getSensorValue(j, i))
+            self._sensorValues.append([1.0] * len(self.ls.sensors))
 
         self.ser = serial.Serial(
             port='COM3',
@@ -25,20 +23,29 @@ class ArduinoConnectorForwardModel(ForwardModel):
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
             bytesize=serial.EIGHTBITS,
-            timeout=0)
+            timeout=10)
 
     def _readLoop(self):
-        while(True):
+        while True:
             line = self.ser.readline()
             match = re.match('Snapshot: ([0-9]+),([0-9]+)', line)
             if match is not None:
                 leds = int(match.group(0))
                 sensors = int(match.group(1))
-                for
+                if leds != len(self.ls.LEDs) or sensors != len(self.ls.sensors):
+                    print("Received wring amount of sensor values: %i / %i; expected %i / %i" % (
+                    leds, sensors, len(self.ls.LEDs), len(self.ls.sensors)))
+                else:
+                    for l in range(leds):
+                        line = self.ser.readline()
+                        vals = line.split(',')
+                        for s in range(sensors):
+                            self._sensorValues[l][s] = float(vals[s]) if s < len(vals) else 0.0
+                    print("received data")
 
     def measureAtPoint(self, x: float, y: float, led: int = -1) -> float:
         # No measurement possible
-        return 0
+        return 0.0
 
     def getSensorValue(self, sensor: int, led: int = -1) -> float:
         if led < 0:
